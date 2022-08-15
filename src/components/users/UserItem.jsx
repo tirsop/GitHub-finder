961f4e3d28fc1b83288cba0538e851585b18b9a9
Tirso Pascual <tpascualgarcia@gmail.com>
2022-08-15 10:29:00 +0900

    style user card i the list

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom"
 
-export default function UserItem() {
+export default function UserItem({ user: { login, avatar_url } }) {
   return (
-    <div>UserItem</div>
+    <div className="card shadow-md compact side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <img className="w-14 h-14 rounded-full drop-shadow-md" src={avatar_url} alt="Profile" />
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{login}</h2>
+          <Link to={`/users/${login}`} className='text-base-content text-opacity-40'>
+            Visit Profiile
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 // components
 import Spinner from "../layouts/Spinner"
+import UserItem from "./UserItem"
 
 export default function UserResults() {
   const [users, setUsers] = useState([])
@@ -31,12 +32,12 @@ export default function UserResults() {
       {users &&
         <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
           {users.map((user) => (
-            <h3>{user.login}</h3>
+            <UserItem key={user.id} user={user} />
           ))}
         </div>
       }
 
-      {loading && <h1> LLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLL</h1>}
+      {loading && <Spinner />}
       {error && <h1>{error}</h1>}
     </>
   )
