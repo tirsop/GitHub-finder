dacfe7b02ff7fc1fcab4fd237915af92f11764aa
Tirso Pascual <tpascualgarcia@gmail.com>
2022-08-15 09:56:30 +0900

    add spinner

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+// components
+import Spinner from "../layouts/Spinner"
 
 export default function UserResults() {
   const [users, setUsers] = useState([])
@@ -25,10 +27,17 @@ export default function UserResults() {
   }, [])
 
   return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {users && users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
+    <>
+      {users &&
+        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+          {users.map((user) => (
+            <h3>{user.login}</h3>
+          ))}
+        </div>
+      }
+
+      {loading && <h1> LLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLLLLllllllLLLLLLLLLLLLLLLLL</h1>}
+      {error && <h1>{error}</h1>}
+    </>
   )
 }
