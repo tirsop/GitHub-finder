ca2ca84f6434faec5a93d8db3d02dda5226f6177
Tirso Pascual <tpascualgarcia@gmail.com>
2022-08-15 17:38:59 +0900

    create setAlert context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 // components
 import Navbar from "./components/layouts/Navbar"
 import Footer from "./components/layouts/Footer"
@@ -7,28 +7,33 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import NotFound from "./pages/NotFound"
 import { GithubProvider } from "./context/github/GithubContext"
+import { AlertProvider } from "./context/alert/AlertContext"
 
 
 
 function App() {
   return (
     <GithubProvider>
-      <BrowserRouter>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
+      <AlertProvider>
 
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notfound' element={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
+        <BrowserRouter>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
 
-          </main>
-          <Footer />
-        </div>
-      </BrowserRouter>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+
+      </AlertProvider>
     </GithubProvider>
   )
 }
