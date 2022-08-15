bc467d9ecaee90347458888b25a16ccffff6ee6e
Tirso Pascual <tpascualgarcia@gmail.com>
2022-08-15 19:12:44 +0900

    set routes for the user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { GithubProvider } from "./context/github/GithubContext"
+import { AlertProvider } from "./context/alert/AlertContext"
 // components
 import Navbar from "./components/layouts/Navbar"
 import Footer from "./components/layouts/Footer"
+import Alert from "./components/layouts/Alert"
 // pages
 import Home from "./pages/Home"
 import About from "./pages/About"
 import NotFound from "./pages/NotFound"
-import { GithubProvider } from "./context/github/GithubContext"
-import { AlertProvider } from "./context/alert/AlertContext"
-import Alert from "./components/layouts/Alert"
+import User from "./pages/User"
 
 
 
@@ -26,6 +27,7 @@ function App() {
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
                 <Route path='/notfound' element={<NotFound />} />
                 <Route path='/*' element={<NotFound />} />
               </Routes>
