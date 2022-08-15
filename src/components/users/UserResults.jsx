78f5c5ffa022a912c92076301111168ce1b3f3ce
Tirso Pascual <tpascualgarcia@gmail.com>
2022-08-15 09:29:04 +0900

    add userResults logic

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react"
 
 export default function UserResults() {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const fetchUsers = async () => {
+    setLoading(true)
+    try {
+      const res = await fetch('https://api.github.com/users')
+      const data = await res.json()
+      setUsers(data)
+    }
+    catch (err) {
+      setError(err.message)
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
   return (
-    <div>UserResults</div>
+    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+      {users && users.map((user) => (
+        <h3>{user.login}</h3>
+      ))}
+    </div>
   )
 }
